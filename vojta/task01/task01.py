b1e959be9b08f2b99de1e0adfc2bbf5793dd2d88
Vojta David <vojtech.david@avast.com>
2021-12-03 00:34:22 +0100

    done task 01

---
+++ vojta/task01/task01.py
@@ -0,0 +1,10 @@
+import csv
+
+with open("input01.txt") as f:
+    counter = 0
+    sequence = [int(x) for x in f.read().split("\n")]
+
+    for previous, current in zip(sequence, sequence[1:]):
+        if current > previous:
+            counter += 1
+    print(counter)
